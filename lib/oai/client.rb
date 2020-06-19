5bcd06fc7ca4806a9d190c1ea9e914536136bed9
Eric James <eric.james@yale.edu>
2020-06-19 15:59:28 -0400

    hardcode metadata_prefix as test

--- lib/oai/client.rb
+++ lib/oai/client.rb
@@ -217,7 +217,7 @@ module OAI
       responseClass.new(do_request(verb, opts)) do |response|
 	puts "R:#{response.inspect}"
         responseClass.new \
-          do_request(verb, :resumption_token => response.resumption_token)
+          do_request(verb, :resumption_token => response.resumption_token, :metadata_prefix => 'marc21')
       end
     end
 
